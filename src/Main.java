df661c6e6056426c4f4ece2b36c93a6a351a05e0
Julia <helli-95.199595@mail.ru>
2026-02-28 16:30:28 +0300

    experiments with number

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {System.out.println("Случайное число от 0 до 1: " + Math.random());}
+    public static void main(String[] args) {
+        System.out.println("Введите первое число:");
+        Scanner scanner = new Scanner(System.in);
+        // Считываем первое число
+        int number1 = scanner.nextInt();
+        System.out.println("Введите второе число:");
+        // Считываем второе число
+        int number2 = scanner.nextInt();
+        // Вывод полученных чисел
+        int result = number1 + number2;
+        System.out.println("сумма: " + result);
+        result = number1 - number2;
+        System.out.println("разность: " + result);
+        result = number1 * number2;
+        System.out.println("произведение: " + result);
+        double resultDouble = (double) number1 / number2;
+        System.out.println("частное: " + resultDouble);
+    }
 }
